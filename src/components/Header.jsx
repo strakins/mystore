a503d9ee3953e217591b309c3ca904861503faab
Blessing Akinola <akinolablessing25@gmail.com>
2024-10-14 11:59:39 +0100

    Add background color of blue to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,26 @@ import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="flex h-32 items-center justify-between md:max-w-4xl md:mx-auto mx-10 uppercase font-semibold ">
-      <Link to='/'>
-        <div className="hover:font-bold cursor-pointer">
-          VegesStore
+    <nav className="bg-blue-500">
+      <section className="flex h-32 items-center justify-between uppercase font-semibold max-w-4xl mx-auto text-white">
+        <Link to='/'>
+          <div className="hover:font-bold cursor-pointer">
+            VegesStore
+          </div>
+        </Link>
+        <div className="flex items-center">
+          <ul className="flex gap-6">
+              <NavLink to='/'> <li className="hover:font-bold cursor-pointer">Home</li></NavLink>
+            <NavLink to='/shop'> <li className="hover:font-bold cursor-pointer">Shop</li></NavLink>
+            <NavLink to='/aboutus'> <li className="hover:font-bold cursor-pointer">About</li></NavLink>
+            <NavLink to='/contact'><li className="hover:font-bold cursor-pointer">Contact</li></NavLink>
+            <NavLink to='/weather'> <li className="hover:font-bold cursor-pointer">Weather</li></NavLink>
+          </ul>
+          <div className="flex items-center gap-1 ml-5">
+            <FaShoppingCart /> [0]
+          </div>
         </div>
-      </Link>
-      <div className="flex items-center">
-        <ul className="flex gap-6">
-            <NavLink to='/'> <li className="hover:font-bold cursor-pointer">Home</li></NavLink>
-           <NavLink to='/shop'> <li className="hover:font-bold cursor-pointer">Shop</li></NavLink>
-           <NavLink to='/aboutus'> <li className="hover:font-bold cursor-pointer">About</li></NavLink>
-           <NavLink to='/contact'><li className="hover:font-bold cursor-pointer">Contact</li></NavLink>
-           <NavLink to='/weather'> <li className="hover:font-bold cursor-pointer">Weather</li></NavLink>
-        </ul>
-        <div className="flex items-center gap-1 ml-5">
-          <FaShoppingCart /> [0]
-        </div>
-      </div>
+      </section>
     </nav>
   )
 }
